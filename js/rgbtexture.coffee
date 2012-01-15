5f2abba936ec6ca2192fdeab09bc1cd60d7519a5
Jon Cooper <joncooper@carbonfive.com>
2012-01-14 16:04:26 -0800

    Texture generated + drawn on canvas with putImageData

--- js/rgbtexture.coffee
+++ js/rgbtexture.coffee
@@ -1,40 +1,28 @@
-drawRGBTexture = (context) ->
-  size = 32
-  bpp  = 4
-  alpha = 255
-  max_intensity = 210
-  min_intensity = 50
-
-  intensity = (pct) ->
-    return Math.floor(min_intensity + (max_intensity - min_intensity))
-
-  square_size = size / 3
-
+makeRGBTexture = (context, size) ->
   imageData = context.createImageData(size, size)
 
-  for x in [0..size]
-    for y in [0..size]
-      base_offset   = (size * bpp * y) + (bpp * x)
-      column        = Math.floor (x / square_size)
-      row           = Math.floor (y / square_size)
-      row_intensity = intensity((2-row) / 2)
+  for x in [0...size]
+    for y in [0...size]
+      # 4 BPP
+      base_offset   = (size * 4 * y) + (4 * x)
+      column        = Math.floor ((x / size) * 2)
+      row           = Math.floor ((y / size) * 2)
 
-      r = if column is 0 then row_intensity else 0
-      g = if column is 1 then row_intensity else 0
-      b = if column is 2 then row_intensity else 0
+      r = if (column is 1 and row is 0) then 220 else 0
+      g = if (column is 1 and row is 1) then 220 else 0
+      b = if (column is 0 and row is 1) then 220 else 0
 
-      imageData[base_offset+0] = r
-      imageData[base_offset+1] = g
-      imageData[base_offset+2] = b
-      imageData[base_offset+3] = alpha
+      imageData.data[base_offset+0] = r
+      imageData.data[base_offset+1] = g
+      imageData.data[base_offset+2] = b
+      imageData.data[base_offset+3] = 255
 
-      console.log(r,g,b,alpha)
-
-  console.log imageData
-  window.imgData = imageData
   return imageData
 
 $ ->
   canvas = $('#container > canvas')[0]
   context = canvas.getContext('2d')
-  drawRGBTexture(context)
+  size = $('#container > canvas').width()
+
+  textureData = makeRGBTexture(context, size)
+  context.putImageData(textureData, 0, 0)
--- js/rgbtexture.js
+++ js/rgbtexture.js
@@ -1,44 +1,33 @@
 (function() {
-  var drawRGBTexture;
+  var makeRGBTexture;
 
-  drawRGBTexture = function(context) {
-    var alpha, b, base_offset, bpp, column, g, imageData, intensity, max_intensity, min_intensity, r, row, row_intensity, size, square_size, x, y;
-    size = 32;
-    bpp = 4;
-    alpha = 255;
-    max_intensity = 210;
-    min_intensity = 50;
-    intensity = function(pct) {
-      return Math.floor(min_intensity + (max_intensity - min_intensity));
-    };
-    square_size = size / 3;
+  makeRGBTexture = function(context, size) {
+    var b, base_offset, column, g, imageData, r, row, x, y;
     imageData = context.createImageData(size, size);
-    for (x = 0; 0 <= size ? x <= size : x >= size; 0 <= size ? x++ : x--) {
-      for (y = 0; 0 <= size ? y <= size : y >= size; 0 <= size ? y++ : y--) {
-        base_offset = (size * bpp * y) + (bpp * x);
-        column = Math.floor(x / square_size);
-        row = Math.floor(y / square_size);
-        row_intensity = intensity((2 - row) / 2);
-        r = column === 0 ? row_intensity : 0;
-        g = column === 1 ? row_intensity : 0;
-        b = column === 2 ? row_intensity : 0;
-        imageData[base_offset + 0] = r;
-        imageData[base_offset + 1] = g;
-        imageData[base_offset + 2] = b;
-        imageData[base_offset + 3] = alpha;
-        console.log(r, g, b, alpha);
+    for (x = 0; 0 <= size ? x < size : x > size; 0 <= size ? x++ : x--) {
+      for (y = 0; 0 <= size ? y < size : y > size; 0 <= size ? y++ : y--) {
+        base_offset = (size * 4 * y) + (4 * x);
+        column = Math.floor((x / size) * 2);
+        row = Math.floor((y / size) * 2);
+        r = column === 1 && row === 0 ? 220 : 0;
+        g = column === 1 && row === 1 ? 220 : 0;
+        b = column === 0 && row === 1 ? 220 : 0;
+        imageData.data[base_offset + 0] = r;
+        imageData.data[base_offset + 1] = g;
+        imageData.data[base_offset + 2] = b;
+        imageData.data[base_offset + 3] = 255;
       }
     }
-    console.log(imageData);
-    window.imgData = imageData;
     return imageData;
   };
 
   $(function() {
-    var canvas, context;
+    var canvas, context, size, textureData;
     canvas = $('#container > canvas')[0];
     context = canvas.getContext('2d');
-    return drawRGBTexture(context);
+    size = $('#container > canvas').width();
+    textureData = makeRGBTexture(context, size);
+    return context.putImageData(textureData, 0, 0);
   });
 
 }).call(this);
