7a7bbc973c8f8d7ab315f6297ca217bf17bc0ff4
Jon Cooper <joncooper@carbonfive.com>
2012-01-26 08:32:10 -0800

    Texture blending in a shader!

--- js/shaders.coffee
+++ js/shaders.coffee
@@ -12,23 +12,50 @@ $ ->
 
   glsl_vs1 = """
 
+varying vec2 pos;
+
 void main() {
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  pos = uv;
 }
 
 """
 
   glsl_fs1 = """
 
+varying vec2 pos;
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+  vec4 t0 = texture2D(tex0, pos);
+  t0.a = 0.5;
+
+  vec4 t1 = texture2D(tex1, pos);
+  t1.a = 0.5;
+
+  vec4 emitColor = mix(t0, t1, 0.5);
+  emitColor.a = 1.0;
+
+  gl_FragColor = emitColor;
 }
 
 """
 
+  uniforms =
+    tex0:
+      type: 't'
+      value: 0
+      texture: THREE.ImageUtils.loadTexture('/shaders/lets-eat-grandma.jpg')
+    tex1:
+      type: 't'
+      value: 1
+      texture: THREE.ImageUtils.loadTexture('/shaders/igloo.jpg')
+
   plane = new THREE.Mesh(
     new THREE.PlaneGeometry(1.80, 1.80, 1, 1)
     new THREE.ShaderMaterial
+      uniforms: uniforms
       fragmentShader: glsl_fs1
       vertexShader: glsl_vs1
   )
--- js/shaders.js
+++ js/shaders.js
@@ -1,6 +1,6 @@
 
   $(function() {
-    var animate, camera, glsl_fs1, glsl_vs1, plane, renderer, scene;
+    var animate, camera, glsl_fs1, glsl_vs1, plane, renderer, scene, uniforms;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(400, 400);
     $('#three').append(renderer.domElement);
@@ -10,9 +10,22 @@
     camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100.0);
     camera.position.z = 100.0;
     scene.add(camera);
-    glsl_vs1 = "\nvoid main() {\n  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n}\n";
-    glsl_fs1 = "\nvoid main() {\n  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);\n}\n";
+    glsl_vs1 = "\nvarying vec2 pos;\n\nvoid main() {\n  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n  pos = uv;\n}\n";
+    glsl_fs1 = "\nvarying vec2 pos;\nuniform sampler2D tex0;\nuniform sampler2D tex1;\n\nvoid main() {\n  vec4 t0 = texture2D(tex0, pos);\n  t0.a = 0.5;\n\n  vec4 t1 = texture2D(tex1, pos);\n  t1.a = 0.5;\n\n  vec4 emitColor = mix(t0, t1, 0.5);\n  emitColor.a = 1.0;\n\n  gl_FragColor = emitColor;\n}\n";
+    uniforms = {
+      tex0: {
+        type: 't',
+        value: 0,
+        texture: THREE.ImageUtils.loadTexture('/shaders/lets-eat-grandma.jpg')
+      },
+      tex1: {
+        type: 't',
+        value: 1,
+        texture: THREE.ImageUtils.loadTexture('/shaders/igloo.jpg')
+      }
+    };
     plane = new THREE.Mesh(new THREE.PlaneGeometry(1.80, 1.80, 1, 1), new THREE.ShaderMaterial({
+      uniforms: uniforms,
       fragmentShader: glsl_fs1,
       vertexShader: glsl_vs1
     }));
